fed69e5fc4ad524e9747bd65d52520f64040c76c
lucastteles <lucasteles_ccb@yahoo.com.br>
2022-09-08 12:33:51 -0300

    CamadaInfra

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+           // builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+           bui
+        }
+    }
+}
